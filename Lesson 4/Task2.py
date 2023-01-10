a37d37d5ea60812a74edd490099f79747bfe5e29
Людмила Ш. Боташева <botasheva.lyudmila@rus.local>
2023-01-10 19:06:31 +0300

    ДЗ 4 Задача 2

---
+++ Lesson 4/Task2.py
@@ -0,0 +1,33 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+# in
+# 54
+# out
+# [2, 3, 3, 3]
+
+# in
+# 9990
+# out
+# [2, 3, 3, 3, 5, 37]
+
+# in
+# 650
+# out
+# [2, 5, 5, 13]
+
+import math 
+
+def prime_numbers(numb):
+    while numb != 1:
+        list_nums = []
+        while numb % 2 == 0:
+            numb /= 2
+            list_nums.append(2)
+        for i in range(3, int(math.sqrt(numb)) + 1, 2):         
+            while numb % i == 0:
+                numb /= i
+                list_nums.append(i)
+    print(list_nums) 
+
+num = int(input("Введите число: "))
+prime_numbers(num)     
